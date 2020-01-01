214e7ef05867ffb6905c94dda13e0a6fc3906dbb
Maxim Sashkin <sqmaxqs@gmail.com>
2020-01-01 14:56:05 +0200

    Add a DataJpa test for UserRepository

---
+++ src/main/resources/org/odessajavaclub/user/adapter/out/jpa/user-repository-test-data.sql
@@ -0,0 +1,11 @@
+insert into USERS(id, first_name, last_name, email, password, role, active)
+values (0, 'Maxim', 'Sashkin', 'msashkin@email.com', '{noop}maxs1234', 'ADMIN', true);
+
+insert into USERS(id, first_name, last_name, email, password, role, active)
+values (1, 'Alexander', 'Pletnev', 'alexp@email.com', '{noop}alexp1234', 'ADMIN', true);
+
+insert into USERS(id, first_name, last_name, email, password, role, active)
+values (2, 'Alexander', 'Bevziuk', 'alexb@email.com', '{noop}alexb1234', 'USER', true);
+
+insert into USERS(id, first_name, last_name, email, password, role, active)
+values (3, 'Oleksii', 'Aleksandrov', 'oleksiia@email.com', '{noop}oleksii1234', 'USER', false);
