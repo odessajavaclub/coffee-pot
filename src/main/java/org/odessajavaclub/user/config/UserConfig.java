989c69912ec3f83e3da40deb60a66bff49acaef5
Maxim Sashkin <sqmaxqs@gmail.com>
2020-01-24 15:12:46 +0200

    Add @ComponentScan to UserConfig

--- src/main/java/org/odessajavaclub/user/config/UserConfig.java
+++ src/main/java/org/odessajavaclub/user/config/UserConfig.java
@@ -4,9 +4,11 @@ import javax.validation.Validation;
 import org.odessajavaclub.shared.Validating;
 import org.odessajavaclub.user.shared.UserIdMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "org.odessajavaclub.user")
 public class UserConfig {
 
   @Bean
