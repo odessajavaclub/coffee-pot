8debf0e887f3ba5ca0224c5299b693afde26af8b
Maxim Sashkin <sqmaxqs@gmail.com>
2020-01-27 16:23:17 +0200

    Remove unnecessary constructor

--- src/main/java/org/odessajavaclub/shared/SelfValidating.java
+++ src/main/java/org/odessajavaclub/shared/SelfValidating.java
@@ -1,14 +1,8 @@
 package org.odessajavaclub.shared;
 
-import javax.validation.ValidatorFactory;
-
 public abstract class SelfValidating<T> {
 
   private final Validating validating;
-
-  protected SelfValidating(ValidatorFactory validatorFactory) {
-    this.validating = new Validating(validatorFactory);
-  }
 
   protected SelfValidating() {
     this.validating = new Validating();
