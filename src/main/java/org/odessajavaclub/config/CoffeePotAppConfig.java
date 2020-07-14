5ccae56e6adb04e191ed4e834efd4c66b92758eb
Maxim Sashkin <msashkin@fastmail.com>
2020-07-14 20:47:21 +0300

    Refactor springfox usage
    Switch to springfox-boot-starter
    Switch to OAS 3.0 instead of Swagger 2.0

--- src/main/java/org/odessajavaclub/config/CoffeePotAppConfig.java
+++ src/main/java/org/odessajavaclub/config/CoffeePotAppConfig.java
@@ -7,16 +7,14 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @ComponentScan(basePackages = "org.odessajavaclub")
-@EnableSwagger2
 public class CoffeePotAppConfig {
 
   @Bean
   Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
+    return new Docket(DocumentationType.OAS_30)
         .select()
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
